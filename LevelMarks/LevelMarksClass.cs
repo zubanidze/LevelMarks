8919e333fb0bc14c7110d5609f456b77b3584be3
zubanidze <49830374+zubanidze@users.noreply.github.com>
2020-04-01 15:09:22 +0300

    version

--- LevelMarks/LevelMarksClass.cs
+++ LevelMarks/LevelMarksClass.cs
@@ -17,20 +17,23 @@ namespace LevelMarks
     [RegenerationAttribute(RegenerationOption.Manual)]
     public class LevelMarksClass : IExternalCommand
     {
-       
+
         public Result Execute(
         ExternalCommandData commandData,
         ref string message,
         ElementSet elements)
         {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiApp.ActiveUIDocument.Document;
+            Form1 ui = new Form1();
+            ui.uiDoc = uiDoc;
+            return (SetLevelAnnotations(doc, ui));             
+        }
+        public Result SetLevelAnnotations(Document doc, Form1 ui)
+        {
             try
             {
-                UIApplication uiApp = commandData.Application;
-                UIDocument uiDoc = uiApp.ActiveUIDocument;
-                Document doc = uiApp.ActiveUIDocument.Document;
-                Form1 ui = new Form1();
-                ui.uiDoc = uiDoc;
-                
                 List<View3D> listOfViews = new List<View3D>();
                 List<string> listOfTeamplates = new List<string>();
                 var textNoteTypeCollector = new FilteredElementCollector(doc).OfClass(typeof(TextNoteType));
@@ -41,7 +44,6 @@ namespace LevelMarks
                     var teamplateId = view.ViewTemplateId;
                     var isViewLocked = view.IsLocked;
                     var isTeamplateIdNotInvalid = view.ViewTemplateId != ElementId.InvalidElementId;
-
                     if (isViewLocked && isTeamplateIdNotInvalid)
                     {
                         var viewTeamplateName = doc.GetElement(view.ViewTemplateId).Name;
@@ -55,7 +57,7 @@ namespace LevelMarks
                 foreach (var name in listOfTeamplates)
                 {
                     var teamplateNode = ui.TreeView.Nodes.Add(name);
-                    
+
                     foreach (var nameOfView in listOfViews)
                     {
                         if (doc.GetElement(nameOfView.ViewTemplateId).Name == teamplateNode.Text)
@@ -64,15 +66,15 @@ namespace LevelMarks
                         }
                     }
                 }
-                foreach(var txtnote in textNoteTypeCollector)
+                foreach (var txtnote in textNoteTypeCollector)
                 {
                     ui.TextNoteTypeComboBox.Items.Add(txtnote.Name);
-                }                
-                foreach(Parameter param in levelCollector.FirstOrDefault().Parameters)
+                }
+                foreach (Parameter param in levelCollector.FirstOrDefault().Parameters)
                 {
                     if (param.IsReadOnly == false && (param.StorageType == StorageType.Integer || param.StorageType == StorageType.String))
                     {
-                        ui.LevelComboBox.Items.Add(param.Definition.Name);  
+                        ui.LevelComboBox.Items.Add(param.Definition.Name);
                     }
                 }
                 var uiResult = ui.ShowDialog();
@@ -83,29 +85,28 @@ namespace LevelMarks
                 var listOfCheckedNames = new List<string>();
                 foreach (TreeNode node in ui.TreeView.Nodes)
                 {
-                    foreach(TreeNode childNode in node.Nodes)
+                    foreach (TreeNode childNode in node.Nodes)
                     {
-                        if(childNode.Checked == true)
+                        if (childNode.Checked == true)
                         {
                             listOfCheckedNames.Add(childNode.Text);
                         }
                     }
                 }
-                
+
                 var listOfChecked3DViews = new List<View3D>();
-                foreach(var name in listOfCheckedNames)
+                foreach (var name in listOfCheckedNames)
                 {
-                    
+
                     foreach (var el in View3DCollector)
-                    {                       
+                    {
                         if (name == el.Name)
-                        {                           
+                        {
                             listOfChecked3DViews.Add(el);
                         }
                     }
                 }
                 var list = new List<TextNote>();
-                MessageBox.Show(listOfChecked3DViews.Count().ToString());
                 foreach (var view in listOfChecked3DViews)
                 {
                     foreach (Level lvl in levelCollector)
@@ -113,27 +114,36 @@ namespace LevelMarks
                         BoundingBoxXYZ boxXYZ = new BoundingBoxXYZ();
                         boxXYZ.Max = new XYZ(100000, 100000, lvl.ProjectElevation);
                         boxXYZ.Min = new XYZ(-100000, -10000, lvl.ProjectElevation);
-                        Outline outline = new Outline(boxXYZ.Min,boxXYZ.Max);                        
+                        Outline outline = new Outline(boxXYZ.Min, boxXYZ.Max);
                         BoundingBoxIntersectsFilter intersectsFilter = new BoundingBoxIntersectsFilter(outline);
                         FilteredElementCollector elementsOnView = new FilteredElementCollector(doc, view.Id).OfClass(typeof(Pipe)).WherePasses(intersectsFilter);
                         Element pipe = elementsOnView.FirstOrDefault();
                         if (elementsOnView.Count() > 0)
-                        {                        
+                        {
                             XYZ origin = new XYZ();
                             var originX = pipe.get_BoundingBox(view).Max.X;
                             var originY = pipe.get_BoundingBox(view).Max.Y;
                             var originZ = lvl.ProjectElevation;
+                            var direction = view.RightDirection;
+                            var leftDirectionx = view.RightDirection.X;
+                            var leftDirectiony = view.RightDirection.Y;
                             if (ui.radioButtonLeft.Checked)
                             {
-                                XYZ origin1 = new XYZ(originX  , originY , originZ);
+                                XYZ leftDirection = new XYZ(leftDirectionx * -6, leftDirectiony * -6, view.RightDirection.Z);
+                                XYZ origin1 = new XYZ(originX, originY, originZ) + leftDirection;
                                 origin = origin1;
                             }
                             else if (ui.radioButtonRight.Checked)
                             {
-                                XYZ origin1 = new XYZ(originX, originY, originZ);
+                                XYZ origin1 = new XYZ(originX, originY, originZ) + direction;
                                 origin = origin1;
                             }
-                            var text = lvl.LookupParameter(ui.LevelComboBox.Text).AsString() + " " + "Эт." + " " + (lvl.ProjectElevation * 0.3048).ToString();
+                            string highMark = lvl.get_Parameter(BuiltInParameter.LEVEL_ELEV).AsValueString();
+                            if (highMark.Length == 5)
+                                highMark = highMark.Insert(2, ".");
+                            else
+                                highMark = highMark.Insert(1, ".");
+                            var text = lvl.LookupParameter(ui.LevelComboBox.Text).AsString() + " " + "Эт." + " " + highMark;
                             ElementId textNoteType = null;
                             foreach (var type in textNoteTypeCollector)
                             {
@@ -141,19 +151,19 @@ namespace LevelMarks
                                     textNoteType = type.Id;
                             }
                             TextNote note = TextNote.Create(doc, view.Id, origin, text, textNoteType);
-                            //MessageBox.Show(origin.ToString());
-                           // MessageBox.Show(note.Text.ToString());
-                        }  
-                        
-                        
+                            
+                            
+                        }
+
+
 
                     }
-                    
-                }                               
+
+                }
                 tr.Commit();
                 TaskDialog.Show("Готово", "Отметки уровней ВК выставлены");
                 return Result.Succeeded;
-        }
+            }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
@@ -161,41 +171,5 @@ namespace LevelMarks
             }
         }
     }
+
 }
-//var elementsInViewCollector = new FilteredElementCollector(doc, view.Id).OfClass(typeof(Pipe));
-//var levelsInViewCollector = new FilteredElementCollector(doc).OfClass(typeof(Level));
-//                    foreach (var elem in elementsInViewCollector)
-//                    {
-//                        var startOffset = ((elem.Location as LocationCurve).Curve as Line).Origin.Z;
-//var endOffset = ((elem.Location as LocationCurve).Curve as Line).Origin.Z + ((elem.Location as LocationCurve).Curve as Line).ApproximateLength;
-//                        foreach (Level level in levelsInViewCollector)
-//                        {
-//                            var heightOfLevel = level.ProjectElevation;                          
-//                            if (startOffset<heightOfLevel && heightOfLevel<endOffset)
-//                            {                               
-//                                XYZ origin1 = new XYZ();
-//var originZ = heightOfLevel;
-//var originX = elem.get_BoundingBox(view).Max.X + 15;
-//var originY = elem.get_BoundingBox(view).Max.X + 15;
-//                                if (ui.radioButtonLeft.Checked)
-//                                {
-//                                    XYZ origin = new XYZ(-originX, -originY, originZ);
-//origin1 = origin;
-//                                }
-//                                else if (ui.radioButtonRight.Checked)
-//                                {
-//                                    XYZ origin = new XYZ(originX, originY, originZ);
-//origin1 = origin;
-//                                }
-//                                var text = level.LookupParameter(ui.LevelComboBox.Text).AsString() + " " + "Эт." + " " + level.LookupParameter("Фасад").AsValueString();
-//ElementId textNoteType = null;
-//                                foreach (var type in textNoteTypeCollector)
-//                                {
-//                                    if (type.Name == ui.TextNoteTypeComboBox.Text)
-//                                        textNoteType = type.Id;
-//                                }                               
-//                                TextNote note = TextNote.Create(doc, view.Id, origin1, text, textNoteType);
-//list.Add(note);                               
-//                            }                  
-//                        }
-//                    }
